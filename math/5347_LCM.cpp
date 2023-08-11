c4ca60ca2eaaf0baac089002bd57468ce20381a2
Song <rmsdud1626@naver.com>
2023-08-12 01:51:16 +0900

    5347_LCM

---
+++ math/5347_LCM.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+long long num1, num2, n;
+
+long long gcd(long long a, long long b)
+{
+	if (b == 0) return a;
+	return gcd(b, a % b);
+}
+
+long long lcm(long long a, long long b)
+{
+	return a * b / gcd(a, b);
+}
+
+int main()
+{
+	cin >> n;
+	while (n--)
+	{
+		cin >> num1 >> num2;
+		cout << lcm(num1, num2) << '\n';
+	}
+	return 0;
+}
